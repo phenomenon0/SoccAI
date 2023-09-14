03c672b841f969ba0f123149716388e2599d624c
Femi Adeniran <66181395+phenomenon0@users.noreply.github.com>
2023-09-14 12:39:05 -0500

    Update steam.py

--- steam.py
+++ steam.py
@@ -115,7 +115,7 @@ Q: {}\n '''
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
+  llm = OpenAI(temperature=0.3, openai_api_key=openai_api_key)
   return (llm(prompt + input_text))
 query =''
 
