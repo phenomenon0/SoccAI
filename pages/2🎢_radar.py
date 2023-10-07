e687c2e44106f40a65d08f89918b5db52f751b20
Femi Adeniran <66181395+phenomenon0@users.noreply.github.com>
2023-10-07 16:23:05 -0500

    Update 2🎢_radar.py
    update func indentation

--- pages/2🎢_radar.py
+++ pages/2🎢_radar.py
@@ -64,6 +64,17 @@ def plot_radar_comparison(player1_data, player2_data, params, low, high, lower_i
         ('https://raw.githubusercontent.com/google/fonts/main/apache/robotoslab/RobotoSlab%5Bwght%5D.ttf',
          FontManager)
     ]
+    title1_text = axs['title'].text(0.01, 0.65, 'Bruno Fernandes', fontsize=25, color='#01c49d',
+                                fontproperties=robotto_bold.prop, ha='left', va='center')
+    title2_text = axs['title'].text(0.01, 0.25, 'Manchester United', fontsize=20,
+                                    fontproperties=robotto_thin.prop,
+                                    ha='left', va='center', color='#01c49d')
+    title3_text = axs['title'].text(0.99, 0.65, 'Kevin De Bruyne', fontsize=25,
+                                    fontproperties=robotto_bold.prop,
+                                    ha='right', va='center', color='#d80499')
+    title4_text = axs['title'].text(0.99, 0.25, 'Manchester City', fontsize=20,
+                                    fontproperties=robotto_thin.prop,
+                                    ha='right', va='center', color='#d80499')
 
     font_managers = [FontManager(url) for url, FontManager in font_urls]
 
@@ -97,17 +108,7 @@ high = [0.37, 0.6, 0.6, 4, 1.2, 10, 8, 1.3, 1.5, 5.5, 5, 5]
 
 bruno_values = [0.25, 0.45, 0.35, 2.5, 0.8, 6, 5, 0.9, 1.2, 4.0, 3.5, 2]
 bruyne_values = [0.32, 0.55, 0.45, 3.0, 1.0, 7, 6, 1.0, 1.4, 4.5, 4.0, 3.8]
-title1_text = axs['title'].text(0.01, 0.65, 'Bruno Fernandes', fontsize=25, color='#01c49d',
-                                fontproperties=robotto_bold.prop, ha='left', va='center')
-title2_text = axs['title'].text(0.01, 0.25, 'Manchester United', fontsize=20,
-                                fontproperties=robotto_thin.prop,
-                                ha='left', va='center', color='#01c49d')
-title3_text = axs['title'].text(0.99, 0.65, 'Kevin De Bruyne', fontsize=25,
-                                fontproperties=robotto_bold.prop,
-                                ha='right', va='center', color='#d80499')
-title4_text = axs['title'].text(0.99, 0.25, 'Manchester City', fontsize=20,
-                                fontproperties=robotto_thin.prop,
-                                ha='right', va='center', color='#d80499')
+
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'Compare two or more players')
   submitted = st.form_submit_button('Submit')
